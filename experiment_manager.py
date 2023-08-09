4ebf0f7496628791d7a5a54ca32c1517fc0ee4e5
Felix Jentzsch <felix.jentzsch@upb.de>
2023-08-09 16:56:04 +0200

    Improve naming, copying from ramdisk

--- experiment_manager.py
+++ experiment_manager.py
@@ -46,8 +46,8 @@ class repository():
     def deploy(self, system, rel_path):
         print("Deploying dependency repo {} to target..".format(self.remote))
         with Connection(system.ssh_target) as c:
-            result = c.run("cd {job_dir} && git clone {remote} {dir} && cd {dir} && git checkout {branch_commit}".format(
-                            job_dir = os.path.join(system.workdir, rel_path), 
+            result = c.run("cd {target_dir} && git clone {remote} {dir} && cd {dir} && git checkout {branch_commit}".format(
+                            target_dir = os.path.join(system.workdir, rel_path), 
                             remote = self.remote, 
                             dir = self.dir, 
                             branch_commit=self.branch_commit))
@@ -65,7 +65,7 @@ class repository():
     
 
 class experiment():
-    def __init__(self, system, tag="job", dependencies=None, environment={}, cmd=None, slurm_options=None, use_ramdisk=False):
+    def __init__(self, system, tag="experiment", dependencies=None, environment={}, cmd=None, slurm_options=None, use_ramdisk=False, copy_back=True):
         self.system = system
         self.tag = tag
         self.dependencies = dependencies
@@ -73,20 +73,21 @@ class experiment():
         self.cmd = cmd
         self.slurm_options = slurm_options
         self.use_ramdisk = use_ramdisk
+        self.copy_back = copy_back
 
-    def prepare_job_dir(self):
+    def prepare_experiment_dir(self):
         time = datetime.now()
-        self.job_dir = time.strftime("%y_%m_%d-%H_%M-") + self.tag
-        self.job_dir_path = os.path.join(self.system.workdir, self.job_dir)
+        self.experiment_name = time.strftime("%y_%m_%d-%H_%M-") + self.tag
+        self.experiment_path = os.path.join(self.system.workdir, self.experiment_name)
         with Connection(self.system.ssh_target) as c:
-            result = c.run("mkdir {}".format(self.job_dir_path))
+            result = c.run("mkdir {}".format(self.experiment_path))
             if not result.ok:
                 print("Error")
 
 
     def deploy_dependencies(self):
         for dependency in self.dependencies:
-            dependency.deploy(self.system, self.job_dir)
+            dependency.deploy(self.system, self.experiment_name)
 
     def prepare_environment(self):
         script = ""
@@ -97,6 +98,7 @@ class experiment():
         script = ""
         for var in self.environment:
             script += "export {var}={value}\n".format(var=var, value=self.environment[var])
+        script += "export EXPERIMENT_DIR={}\n".format(self.experiment_path)
         script_experiment = io.StringIO(script)
 
         if self.system.use_slurm:
@@ -115,18 +117,18 @@ class experiment():
             for command in self.slurm_options["post_commands"]:
                 script += "{}\n".format(command)
 
-            if self.use_ramdisk:
-                script += "cp -dfR {ramdisk_path}/. {job_dir_path}\n".format(ramdisk_path=self.system.ramdisk_path, job_dir_path=self.job_dir_path)
+            if self.use_ramdisk and self.copy_back:
+                script += "cp -dfR {ramdisk_path}/. {experiment_path}\n".format(ramdisk_path=self.system.ramdisk_path, experiment_path=self.experiment_path)
 
             script_slurm = io.StringIO(script)
 
         with Connection(self.system.ssh_target) as c:
-            result = c.put(script_system, os.path.join(self.job_dir_path, "system_env.sh"))
-            result = c.put(script_experiment, os.path.join(self.job_dir_path, "experiment_env.sh"))
+            result = c.put(script_system, os.path.join(self.experiment_path, "env_system.sh"))
+            result = c.put(script_experiment, os.path.join(self.experiment_path, "env_experiment.sh"))
 
             if self.system.use_slurm:
-                result = c.put(script_slurm, os.path.join(self.job_dir_path, "slurm_script.sh"))
-                result = c.put(io.StringIO(json.dumps(self.slurm_options)), os.path.join(self.job_dir_path, "slurm_options.json")) # just for logging purposes
+                result = c.put(script_slurm, os.path.join(self.experiment_path, "slurm_script.sh"))
+                result = c.put(io.StringIO(json.dumps(self.slurm_options)), os.path.join(self.experiment_path, "slurm_options.json")) # just for logging purposes
 
 
     def execute_cmd(self):
@@ -136,18 +138,18 @@ class experiment():
                 for option in self.slurm_options:
                     if option not in ["pre_commands", "post_commands"]:
                         command += "{option}={value} ".format(option=option, value=self.slurm_options[option])
-                command += "--job-name={} ".format(self.job_dir)
+                command += "--job-name={} ".format(self.experiment_name)
                 command += "slurm_script.sh"
             else:
                 command = self.cmd
 
             print("Running command: {}".format(command))
-            result = c.run("cd {job_dir} && source system_env.sh && source experiment_env.sh && {cmd}".format(job_dir = self.job_dir_path, cmd = command))
+            result = c.run("cd {experiment_path} && source env_system.sh && source env_experiment.sh && {cmd}".format(experiment_path = self.experiment_path, cmd = command))
             if not result.ok:
                 print("Error")
 
     def launch(self):
-        self.prepare_job_dir()
+        self.prepare_experiment_dir()
         self.deploy_dependencies()
         self.prepare_environment()
         self.execute_cmd()
