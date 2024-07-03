6a71d279a7e846fecfc1f3615811905241e035fa
Felix Jentzsch <felix.jentzsch@upb.de>
2024-07-03 12:41:34 +0000

    check-in harness vivado projects

---
+++ power_measurement/harness_source/harness_source.srcs/sources_1/new/harness_source.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/22/2023 03:20:04 PM
+// Design Name: 
+// Module Name: harness_source
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module harness_source(
+    input as,
+    output [8:0] abc,
+    output ss
+    );
+endmodule
