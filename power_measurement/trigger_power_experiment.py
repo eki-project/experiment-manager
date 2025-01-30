d40802c4e59b8c353d740929b7dfb97e9d63827c
Felix Jentzsch <felix.jentzsch@upb.de>
2025-01-30 20:14:50 +0000

    Check-in updated measurement script

--- power_measurement/trigger_power_experiment.py
+++ power_measurement/trigger_power_experiment.py
@@ -1,5 +1,6 @@
 import fcntl
 import time
+from datetime import datetime
 import sys
 import os
 import re
@@ -9,6 +10,11 @@ import pandas as pd
 from fabric import Connection
 from patchwork import transfers
 import vxi11
+
+def log(str):
+    current_time = datetime.now()
+    str_date_time = current_time.strftime("%d-%m_%H-%M: ")
+    print(str_date_time + str)
 
 def board_power_on():
     psu = vxi11.Instrument("131.234.250.74")
@@ -35,18 +41,23 @@ if __name__ == '__main__':
     board_on = False
     # search for bitstreams on pfs job dir w/o measurement report
     for experiment in os.listdir("/mnt/pfs/hpc-prf-radioml/felix/jobs"):
-        if os.path.isdir("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bench_results/bitstreams"):
-            for file in os.listdir("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bench_results/bitstreams"):
+        if os.path.isdir("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bitstreams"):
+            for file in os.listdir("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bitstreams"):
                 if re.match(r"run_\d*.bit", file):
-                    if not os.path.isfile("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bench_results/bitstreams/" + file.replace(".bit", "_load.xlsx")):
+                    if not os.path.isfile("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bitstreams/" + file.replace(".bit", "_load.xlsx")):
                         # un-measured bistream detected
-                        print("Running power measurement for: %s/%s"%(experiment, file))
+                        log("Running power measurement for: %s/%s"%(experiment, file))
                         if not board_on:
-                            print("Powering on board")
+                            log("Powering on board")
                             board_power_on()
                             board_on = True
-                            print("Waiting for 120s")
+                            log("Waiting for 120s")
                             time.sleep(120)
+
+                        settings = {}
+                        with open(os.path.join("/mnt/pfs/hpc-prf-radioml/felix/jobs/", experiment , "bitstreams/", file.replace(".bit", "_settings.json")), "r") as f:
+                                settings = json.load(f)
+                        log("Loaded settings json file")
 
                         # different frequencies?
                         # w/ and w/o external psu (due to instabilities)
@@ -68,7 +79,7 @@ if __name__ == '__main__':
                                     "title" : "idle",
                                     "functions" : [
                                                 {"name": "run_idle", "args" : [], 
-                                                "kwargs" : {"frequency": 200}
+                                                "kwargs" : {"frequency": settings["freq_mhz"]}
                                                 }
                                     ],
                                     "num_runs" : 1,
@@ -78,7 +89,7 @@ if __name__ == '__main__':
                                     "title" : "load",
                                     "functions" : [
                                                 {"name": "run_free", "args" : [], 
-                                                "kwargs" : {"frequency": 200}
+                                                "kwargs" : {"frequency": settings["freq_mhz"]}
                                                 }
                                     ],
                                     "num_runs" : 1,
@@ -86,11 +97,11 @@ if __name__ == '__main__':
                                 }
                             ]
                         }
-                        with open("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bench_results/bitstreams/" + file.replace(".bit", "_paf.json"), "w") as f:
+                        with open("/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bitstreams/" + file.replace(".bit", "_paf.json"), "w") as f:
                             json.dump(experiment_flow, f, indent=2)
 
-                        print("Copying experiment flow cfg, bitstream, hwh to board")
-                        source_path = "/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bench_results/bitstreams/"
+                        log("Copying experiment flow cfg, bitstream, hwh to board")
+                        source_path = "/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bitstreams/"
                         target_path = "/home/xilinx/power_measurement/experiments/%s/"%(experiment)
                         prefix = file.replace(".bit", "")
                         with Connection("xilinx@ceg-307.cs.upb.de") as c:
@@ -99,46 +110,56 @@ if __name__ == '__main__':
                             c.put(source_path + prefix + ".hwh", target_path + prefix + ".hwh")
                             c.put(source_path + prefix + "_paf.json", target_path + prefix + "_paf.json")
 
-                            print("Starting PAF")
+                            log("Starting PAF")
                             c.run("sudo bash -c 'cd /home/xilinx/power_measurement && source /etc/profile.d/xrt_setup.sh && source /etc/profile.d/pynq_venv.sh && python paf/paframework/paframework.py {}'".format(target_path + prefix + "_paf.json"),
                                   pty=True)
                             
-                            print("Saving output .xlsx files")
+                            log("Saving output .xlsx files")
                             c.run("mv /home/xilinx/power_measurement/idle_run1.xlsx /home/xilinx/power_measurement/experiments/%s/%s_idle.xlsx"%(experiment, prefix))
                             c.run("mv /home/xilinx/power_measurement/load_run1.xlsx /home/xilinx/power_measurement/experiments/%s/%s_load.xlsx"%(experiment, prefix))
                             c.get(target_path + prefix + "_idle.xlsx", source_path + prefix + "_idle.xlsx")
                             c.get(target_path + prefix + "_load.xlsx", source_path + prefix + "_load.xlsx")
 
-                        print("Parsing results and writing to compact log")
+                        log("Parsing results and writing to compact log")
                         df = pd.read_excel(source_path + prefix + "_idle.xlsx")
-                        power_pl_idle = round(df["0V85_power"].mean() * 0.001, 3)
+                        power_pl_ps_idle = round(df["0V85_power"].mean() * 0.001, 3)
                         power_total_idle = round(df["total_power"].mean() * 0.001, 3)
                         df = pd.read_excel(source_path + prefix + "_load.xlsx")
-                        power_pl_load = round(df["0V85_power"].mean() * 0.001, 3)
+                        power_pl_ps_load = round(df["0V85_power"].mean() * 0.001, 3)
                         power_total_load = round(df["total_power"].mean() * 0.001, 3)
 
-                        power_log_path = "/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/bench_results/power_measure.json"
+                        # compute further power metrics from measurements
+                        power_pl_dyn_load_comp = round(power_pl_ps_load - power_pl_ps_idle, 3)
+                        power_total_dyn_load_comp = round(power_total_load - power_total_idle, 3)
+
+                        power_log_path = "/mnt/pfs/hpc-prf-radioml/felix/jobs/" + experiment + "/power_measure.json"
                         if os.path.isfile(power_log_path):
                             with open(power_log_path, "r") as f:
-                                log = json.load(f)
+                                logfile = json.load(f)
                         else:
-                            log = []
+                            logfile = []
 
-                        log.append({
+                        logfile.append({
                             "run_id": int(prefix.replace("run_","")),
-                            "power_pl_idle": power_pl_idle,
-                            "power_total_idle": power_total_idle,
-                            "power_pl_load": power_pl_load,
-                            "power_total_load": power_total_load
+                            "output": {
+                                "power": {
+                                    "power_pl_ps_idle": power_pl_ps_idle,
+                                    "power_total_idle": power_total_idle,
+                                    "power_pl_ps_load": power_pl_ps_load,
+                                    "power_total_load": power_total_load,
+                                    "power_total_dyn_load_comp": power_total_dyn_load_comp,
+                                    "power_pl_dyn_load_comp": power_pl_dyn_load_comp
+                                }
+                            }
                         })
 
                         with open(power_log_path, "w") as f:
-                            json.dump(log, f, indent=2)
+                            json.dump(logfile, f, indent=2)
 
     if board_on:
-        print("Powering board off after 30s wait")
+        log("Powering board off after 30s wait")
         time.sleep(30)
         board_power_off()
-        print("Done")
+        log("Done")
     else:
-        print("No un-measured bitstreams found in experiment dirs")
+        log("No un-measured bitstreams found in experiment dirs")
